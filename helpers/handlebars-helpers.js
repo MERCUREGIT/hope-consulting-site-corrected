4a0b5488f4f695b4495802d2d195641061e95fc2
mercuregit <ngumbukafon@gmail.com>
2021-03-06 21:16:15 +0100

    addition of generateTime handle bar helper to help reduce the size of text displayes on immobilier cards

--- helpers/handlebars-helpers.js
+++ helpers/handlebars-helpers.js
@@ -10,6 +10,9 @@
 
      generateTime: (date, format) => {
          return moment(date).format(format)
+     },
+     shortenText: (description, length) => {
+         return description.substring(0, length)
      }
 
  }
