2a4c20118abf61ca2ac889f7ec67dd39a7c5070e
mercuregit <ngumbukafon@gmail.com>
2021-03-06 21:18:42 +0100

    correction of browser support styling scripts

--- public/js/functions.js
+++ public/js/functions.js
@@ -5,7 +5,7 @@
 	$(window).on('load', function () { // makes sure the whole site is loaded
 		'use strict';
 		$('[data-loader="circle-side"]').fadeOut(); // will first fade out the loading animation
-		$('#preloader').delay(350).fadeOut('slow'); // will fade out the white DIV that covers the website.
+		$('#preloader').delay(0).fadeOut('slow'); // will fade out the white DIV that covers the website.
 	})
 
 	/* Animating the navbar toggle*/
