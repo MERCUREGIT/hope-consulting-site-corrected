af1022bb3cd1e455fbff7ed46a35eaffd613d844
mercuregit <ngumbukafon@gmail.com>
2021-03-06 21:21:55 +0100

    addition of cards ad jumbotrons partials

--- index.js
+++ index.js
@@ -4,66 +4,63 @@ const path = require('path');
 const Handlebars = require('handlebars')
 const { allowInsecurePrototypeAccess } = require('@handlebars/allow-prototype-access')
 const exphbs = require('express-handlebars');
-const mongoose = require('mongoose');
+const cors = require('cors');
+const compression = require('compression');
 const bodyParser = require('body-parser');
 const methodOverride = require('method-override');
 const upload = require('express-fileupload');
-const session = require('express-session');
-const flash = require('connect-flash');
-const passport = require('passport')
+const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
+const morgan = require('morgan');
+const helmet = require('helmet')
+const http2 = require('http2');
 
-mongoose.connect('mongodb+srv://hopetest:1234@cluster0.mcou4.mongodb.net/hope-consulting?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true }).then((db) => {
+// mongoose.connect('mongodb+srv://hopetest:1234@cluster0.mcou4.mongodb.net/hope-consulting?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true }).then((db) => {
+    //     console.log("MONGO CONNECTED");
+    // }).catch(error => console.log(error));
+// app.use(helmet());
+mongoose.connect('mongodb://localhost:27017/hope-consulting?readPreference=primary&appname=MongoDB%20Compass&ssl=false', { useNewUrlParser: true, useUnifiedTopology: true }).then((db) => {
     console.log("MONGO CONNECTED");
 }).catch(error => console.log(error));
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public/')));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-
 app.use(upload());
+app.use(compression());
 
-// method override
+//  list of domains white listed 
+// const whiteList =['http://localhost:8000','http://localhost:3000/','https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css'];
+// const corsOptions={
+//     origin: function(origin, callback) {
+//         if(whiteList.indexOf(origin) !== -1){
+//             callback(null, true);
+//         }else{
+//             callback(new Error('Not allowed by CORS'));
+//         }
+//     }
+// }
+app.use(cors());
+
+
+app.use(morgan('tiny'));
 app.use(methodOverride('_method'));
 
-const { select, generateTime } = require('./helpers/handlebars-helpers');
-
-// load engines
+const { select, generateTime, shortenText } = require('./helpers/handlebars-helpers');
 app.engine('handlebars', exphbs({
     defaultLayout: 'home',
     handlebars: allowInsecurePrototypeAccess(Handlebars),
-    helpers: { select: select, generateTime: generateTime }
+    helpers: { select: select, generateTime: generateTime, shortenText:shortenText }
 }));
 app.set('view engine', 'handlebars');
 
-app.use(session({
-    secret: 'heckmack123',
-    resave: true,
-    saveUninitialized: true,
-}));
 
-app.use(flash());
-
-//pasport
-app.use(passport.initialize());
-app.use(passport.session());
-
-
-// local variables using middleware
-
-app.use((req, res, next) => {
-    res.locals.success_message = req.flash('success_message');
-    res.locals.user = req.user || null;
-    next();
-})
 
 // load routes
-
 const home = require('./routes/home/index');
 const admin = require('./routes/admin/index');
 
 // Use routes
-
 app.use('/', home);
 app.use('/admin', admin);
 
@@ -73,3 +70,4 @@ const port = process.env.PORT || 8000
 app.listen(port, () => {
     console.log(`Listening on port ${8000}`);
 });
+
