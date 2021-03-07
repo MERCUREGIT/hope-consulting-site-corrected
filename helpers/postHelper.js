133fa79f83db876dbdfe5ff1b51b6edac6fde043
mercuregit <ngumbukafon@gmail.com>
2021-03-07 23:08:42 +0100

    update of post helper to support job offer creation

--- helpers/postHelper.js
+++ helpers/postHelper.js
@@ -1,6 +1,6 @@
 module.exports = {
 
-    postHelper: (req, res, dbmodel, offerStatus, redirectPage)=>{
+    postHelper: (req, res, dbmodelInstance, offerStatus, redirectPage)=>{
         if(req.files){
             console.log(req.files.file_contact)
             let file = req.files.file_contact;
@@ -13,24 +13,60 @@ module.exports = {
             fileName = '';
         }
            try{
-            dbmodel.offre=offerStatus;
-            dbmodel.vendor_name= req.body.name_contact;
-            dbmodel.phone_number=req.body.tel_contact;
-            dbmodel.email= req.body.email_contact;
-            dbmodel.loaction=req.body.ville_contact;
-            dbmodel.topic=req.body.object_contact;
-            dbmodel.description= req.body.message_contact;
-            dbmodel.image= fileName;
-            dbmodel.area= req.body.area;
-            dbmodel.Price = req.body.price;
-            dbmodel.house = req.body.house;
+            dbmodelInstance.offre=offerStatus;
+            dbmodelInstance.vendor_name= req.body.name_contact;
+            dbmodelInstance.phone_number=req.body.tel_contact;
+            dbmodelInstance.email= req.body.email_contact;
+            dbmodelInstance.loaction=req.body.ville_contact;
+            dbmodelInstance.topic=req.body.object_contact;
+            dbmodelInstance.description= req.body.message_contact;
+            dbmodelInstance.image= fileName;
+            dbmodelInstance.area= req.body.area;
+            dbmodelInstance.Price = req.body.price;
+            dbmodelInstance.house = req.body.house;
            }
            catch(err){
                console.log(err)
            }
-           dbmodel.save().then(()=>{
+           dbmodelInstance.save().then(()=>{
                console.log(req)
                res.redirect(redirectPage)
     });
+    },
+    employmentOffer: (req, res,dbmodelInstance, isOffer, redirectUrl) => {
+
+        if (isOffer) {
+            try{
+                dbmodelInstance.entreprise= req.body.entreprise;
+                dbmodelInstance.site_web= req.body.site_web;
+                dbmodelInstance.phone_number=req.body.phone_number;
+                dbmodelInstance.email= req.body.email;
+                dbmodelInstance.category=req.body.category;
+                dbmodelInstance.type=req.body.type;
+                dbmodelInstance.description= req.body.message_contact;
+                dbmodelInstance.poste= req.body.poste;
+                dbmodelInstance.date_expiration = req.body.date_expiration;
+                dbmodelInstance.location = req.body.location;
+                dbmodelInstance.description = req.body.description;
+               }
+               catch(err){
+                   console.log(err)
+            }
+        }
+        else if(!isOffer) {
+            if(req.files){
+                console.log(req.files.cv)
+                let file = req.files.cv;
+                var fileName = Date.now() + '-' + file.name;
+                let dirUploads = './public/uploads/';
+                file.mv(dirUploads + fileName, err => {
+                    if (err) throw err;
+                });
+            }else {
+                fileName = '';
+            }
+            // incomplete
+        }
+        dbmodelInstance.save().then(()=>res.redirect(redirectUrl))
     }
 }
