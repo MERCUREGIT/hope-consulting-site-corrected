38fe647ae21fedf0788b75986b681d475b358c88
mercuregit <ngumbukafon@gmail.com>
2021-03-08 15:22:07 +0100

    candidadte model

--- models/Candidature.js
+++ models/Candidature.js
@@ -4,26 +4,16 @@ const Schema = mongoose.Schema;
 
 const CandidatureSchema = new Schema({
 
-    nom:{
-       type:String,
-       required:true
-    },
-    telephone:{
-       type:String,
-       required:true
-    },
-    email:{
-       type:String,
-    },
-    desciption:{
-       type:String,
-    },
-    date:{
-       type:Date,
-       default:Date.now()
-    }
-
+   nom: {type: String,required: true,},
+   email: { type: String, },
+   experience:{type:Number},
+   phone_number: {type: String,required: true,},
+   category: {type: String,required: true},
+   type: {type:String, required:true},
+   location: {type:String, required:true},
+   date: {type: Date,default: Date.now()},
+   description: { type: String, required: true, },
+   cv:{type:String, required: true}
 }, { usePusEach: true });
-
-
 module.exports = mongoose.model('Candidature', CandidatureSchema);
+
