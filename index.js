3e6248be07f93ff139c517098982282ab5cbd3a5
mercuregit <ngumbukafon@gmail.com>
2021-03-08 15:23:45 +0100

    update of index file

--- index.js
+++ index.js
@@ -33,6 +33,7 @@ else
 
 
 app.use(express.static(path.join(__dirname, 'public/')));
+app.use("/cv",express.static(path.join(__dirname, 'public/uploads')));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(upload());
