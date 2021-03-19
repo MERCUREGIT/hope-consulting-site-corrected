e5baa546d2f13ad37f499e3be53591c95ba643cb
mercuregit <ngumbukafon@gmail.com>
2021-03-19 16:57:50 +0100

    route update and removal of cors

--- routes/admin/index.js
+++ routes/admin/index.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const fs = require('fs');
-const dashboard_url = "https://hope-consulting-dashboard.netlify.app/";
+const dashboard_url = "https://hope-consulting-admin.netlify.app/";
 
 
-const delRoutes = require('./delete')(router, fs, dashboard_url);
-const postRoutes = require('./post')(router, dashboard_url);
-const getRoutes = require('./get')(router, dashboard_url);
+require('./delete')(router, fs, dashboard_url);
+require('./post')(router, dashboard_url);
+require('./get')(router, dashboard_url);
 
 
 module.exports = router;
